1dfefc8690b97a554269e27ee09c27d37e818193
AkankshaAttavar <151825680+AkankshaAttavar@users.noreply.github.com>
2026-05-02 23:31:25 +0300

    Section 7 - checkboxes, dropdowns, hands on practice

---
+++ src/main/java/PracticeTest.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class PracticeTest {
+
+    public static void main (String[] args){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+        driver.findElement(By.id("checkBoxOption1")).click();
+        Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+
+        //Getting count of checkboxes
+        System.out.println(driver.findElements(By.xpath("//input[@type = 'checkbox']")).size());
+
+        driver.quit();
+    }
+
+}
