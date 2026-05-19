dd978a2b9b34cc4f8c27342f33f1078d73bda821
AkankshaAttavar <151825680+AkankshaAttavar@users.noreply.github.com>
2026-05-20 00:43:44 +0300

    Section 10 Assignment 45- Nested frames

---
+++ src/main/java/sc10assig5.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sc10assig5 {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Nested Frames")).click();
+        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame("frame-middle");
+        System.out.println(driver.findElement(By.id("content")).getText());
+
+    }
+
+}
+
